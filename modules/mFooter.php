1c2074a8f421274a133de09ecf73ccd9c5aac120
thienhoang2510 <50736218+thienhoang2510@users.noreply.github.com>
2021-01-08 22:55:13 +0700

    Add files via upload

---
+++ modules/mFooter.php
@@ -0,0 +1,86 @@
+    <!-- Footer -->
+  <footer class="py-2 bg-info">
+    <div class="container-fluid">
+      <p class="m-0 text-center text-white">Copyright &copy; Student of HCMUS 2020</p>
+
+    </div>
+    <!-- /.container -->
+    <div class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Địa chỉ chúng tôi</h2>
+                            <div class="contact-info">
+                                <p><i class="fa fa-map-marker"></i>227 Nguyễn văn cừ Q.5,HCM</p>
+                                <p><i class="fa fa-envelope"></i>Shopgiay@gmai.com</p>
+                                <p><i class="fa fa-phone"></i>+0355-286-781</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Theo dõi chúng tôi</h2>
+                            <div class="contact-info">
+                                <div class="social">
+                                    <a href=""><i class="fab fa-twitter"></i></a>
+                                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                    <a href=""><i class="fab fa-instagram"></i></a>
+                                    <a href=""><i class="fab fa-youtube"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Thông tin cửa hàng</h2>
+                            <ul>
+                                <li><a href="#">Về chúng tôi</a></li>
+                                <li><a href="#">Chính sách bảo mật</a></li>
+                                <li><a href="#">Điều khoản & Điều kiện</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-widget">
+                            <h2>Chính sách mua hàng</h2>
+                            <ul>
+                                <li><a href="#">Chính sách đổi trả</a></li>
+                                <li><a href="#">Chính sách vận chuyển</a></li>
+                                <li><a href="#">Chính sách hoàn trả</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row payment align-items-center">
+                    <div class="col-md-6">
+                        <div class="payment-method">
+                            <h2>Cổng thanh toán:</h2>
+                            <img src="imgs/icons/payment-method.png" alt="Payment Method" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        <!-- Footer Bottom Start -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 copyright">
+                        <p>Copyright &copy; <a href="https://htmlcodex.com">HTML Codex</a>. All Rights Reserved</p>
+                    </div>
+
+                    <div class="col-md-6 template-by">
+                        <p>Template By <a href="https://htmlcodex.com">HTML Codex</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </footer>
---
+++ modules/mHeader.php
@@ -0,0 +1,155 @@
+<?php
+$layout = 1;
+if (isset($_GET["layout"]))
+$layout = $_GET["layout"];
+$side = 1;
+if (isset($_GET["side"]))
+$side = $_GET["side"];
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><strong>Shoes Store</strong></a>
+<a href="index.html">
+                                <img src="imgs/logo/logo.png" alt="Logo">
+                            </a>
+    <!-- mobile button  -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- /mobile button  -->
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <!--active class  -->
+          <a class="nav-link<?php if($layout==1&&$side!=2) echo " active"; ?>" href="index.php">Trang chủ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link<?php if($layout==1&&$side==2) echo " active"; ?>" href="index.php?layout=1&side=2&content=5">Sản phẩm</a>
+        </li>
+      </ul>
+    </div>
+
+
+    <div class="nav-icons-container d-flex align-items-center justify-content-between">
+      <div class="nav-icon">
+
+
+        <form class="input-group" action="index.php?layout=1&side=2&content=5" method="post">
+          <input type="text" class="form-control" placeholder="Search">
+          <div class="input-group-append">
+            <button class="btn btn-info" type="submit"><i class="fa fa-search"></i></button>
+          </div>
+        </form>
+
+
+      </div>
+
+
+
+      <?php
+      if (isset($_SESSION["MaTaiKhoan"])) {
+      ?>
+
+        <div class="username"><?php echo "Hello, " . $_SESSION["TenHienThi"];  ?></div>,
+        <div class="nav-icon">
+
+          <div class="dropdown">
+            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-user"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <form class="p-2 text-secondary" action="">
+
+
+
+                <?php
+                if ($_SESSION["MaLoaiTaiKhoan"] == 2) {
+                ?>
+                  <a href="./admin/index.php">Quyền Admin</a>
+                <?php
+                }
+
+                ?>
+                <div class="dropdown-divider mt-0"></div>
+
+                <a href="./pages/dangnhap/xlDangXuat.php">Đăng xuất </a>
+              </form>
+
+
+
+            </div>
+          </div>
+        </div>
+
+      <?php
+      } else {
+      ?>
+        <div class="nav-icon">
+
+          <div class="username">Người dùng</div>
+
+        </div>
+
+        <div class="nav-icon">
+          <div class="dropdown">
+            <a href="" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-user"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <form class="p-2 text-secondary" method="POST" action="./pages/dangnhap/xlDangNhap.php">
+                <div class="form-group">
+                  <input name="username" type="text" class="form-control" placeholder="Nhập tên tài khoản" required>
+                </div>
+                <div class="form-group">
+                  <input name="password" type="password" class="form-control" placeholder="Nhập mật khẩu" required>
+                </div>
+                <button type="submit" class="btn btn-primary my-2">Đăng nhập!</button>
+              </form>
+              <div class="dropdown-divider mt-0"></div>
+              <a class="dropdown-item" href="./index.php?layout=3">Đăng ký tài khoản</a>
+              <a class="dropdown-item" href="">Quên mật khẩu</a>
+
+            </div>
+          </div>
+        </div>
+
+      <?php
+
+      }
+
+      ?>
+
+
+
+      <?php
+      if (isset($_SESSION["sosanpham"])) #neu co san pham trong gio
+      {
+        $soSanPham = $_SESSION["sosanpham"];
+      } else {
+        $soSanPham = 0;
+      }
+      #hien thi badge so luong sp ở dưới
+      ?>
+
+
+      <div class="nav-icon">
+
+        <div class="dropdown">
+          <a href="./index.php?layout=2">
+            <i class="fa fa-shopping-cart"></i>
+            <span class="badge"><?php echo $soSanPham ?></span>
+          </a>
+
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  </div>
+
+</nav>
