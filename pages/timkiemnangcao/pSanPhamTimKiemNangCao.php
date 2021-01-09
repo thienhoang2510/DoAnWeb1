da24467a86ba20aaea9aa14c2a3fd7729484d186
Grinfs <47818920+grinfs@users.noreply.github.com>
2021-01-10 04:34:09 +0700

    Add files via upload

--- pages/timkiemnangcao/pSanPhamTimKiemNangCao.php
+++ pages/timkiemnangcao/pSanPhamTimKiemNangCao.php
@@ -1,8 +1,7 @@
 <?php
 if (isset($_POST['searchTxt'])) { //nội dung tìm kiếm
     $value = $_POST['searchTxt'];
-    $term = mysql_real_escape_string($_REQUEST['term']);
-    $sql = "SELECT * FROM sanpham WHERE BiXoa = 0 AND TenSanPham LIKE '%".$term."%'";
+    $sql = "select * from sanpham where TenSanPham like '%$value%'";
     $result = DataProvider::ExecuteQuery($sql);
 } else {
     $sql = "SELECT * FROM sanpham WHERE BiXoa = 0";
@@ -13,10 +12,10 @@ if (isset($_POST['searchTxt'])) { //nội dung tìm kiếm
 <!-- BEGIN RESULT -->
 <div class="wrapper m-3">
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-12">
             <h3><i class="fa fa-search"></i> Kết quả</h3>
             <hr>
-
+            </div>
         </div>
     </div>
     <hr>
@@ -25,18 +24,12 @@ if (isset($_POST['searchTxt'])) { //nội dung tìm kiếm
         <h4>Danh sách sản phẩm</h4>
         <hr class="w-50">
         <div class="row" id="data">
-
             <?php
             while ($row = mysqli_fetch_array($result)) {
                 $itemcard = new ItemCard($row["MaSanPham"], $row["HinhURL"], $row["TenSanPham"], $row["GiaSanPham"]);
                 $itemcard->create();
             }
             ?>
-
-
         </div>
     </div>
-
-
-
 </div>
