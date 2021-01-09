c025b4e50ee29ced635a23bc50a66efa3e1c7cc6
phamgialam2000 <75935354+phamgialam2000@users.noreply.github.com>
2021-01-09 22:22:29 +0700

    Update pTaoTaiKhoan.php

--- pages/dangky/pTaoTaiKhoan.php
+++ pages/dangky/pTaoTaiKhoan.php
@@ -1,45 +1,112 @@
-<h1> Tạo tài khoản mới</h1>
+
+
+<style type="text/css">
+  *{ 
+            margin: 2px;
+            padding: 2px;
+            box-sizing: border-box;
+    }
+    .right {
+            text-align: right;
+        }
+        .center {
+            border-style: solid;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        
+</style>
+
+<div style="text-align: center">
+            
+            <pre>
+            
+            
+            
+            <h1>.:TẠO TÀI KHOẢN MỚI:.</h1>
+            </pre>
+            <hr>
+        </div>
+<div>
 <form action="pages/dangky/xlTaoTaiKhoan.php" method="post" onsubmit="return KiemTra()">
-   <div>
-       <span class="lable">Tên đăng nhập:</span>
-       <input type="text" id="us" name="us"/>
-       <span class="err" id="eUS"></span>
-   </div>
-   <div>
-       <span class="lable">Mật khẩu:</span>
-       <input type="password" id="ps" name="ps"/>
-       <span class="err" id="ePS"></span>
-   </div>
-   <div>
-       <span class="lable">Nhập lại mật khẩu:</span>
-       <input type="password" id="us" name="rps"/>
-       <span class="err" id="eRPS"></span>
-   </div>
-   <div>
-       <span class="lable">Tên hiển thị:</span>
-       <input type="text" id="name" name="name"/>
-       <span class="err" id="eNAME"></span>
-   </div>
-   <div>
-       <span class="lable">Địa chỉ:</span>
-       <input type="text" id="add" name="add"/>
-       <span class="err" id="eADD"></span>
-   </div>
-   <div>
-       <span class="lable">Điện thoại:</span>
-       <input type="text" id="tel" name="tel"/>
-       <span class="err" id="eTEL"></span>
-   </div>
-   <div>
-       <span class="lable">Email:</span>
-       <input type="text" id="mail" name="mail"/>
-       <span class="err" id="eMail"></span>
-   </div>
-   <div>
-       <span class="lable"></span>
-       <input type="submit" value="Đăng ký"/>     
-   </div>
-</form>
+
+    <table style="width: auto" class="center">
+                    <tr>
+                        <th class="right">
+                        <div>
+                            <span class="lable">Tên đăng nhập:</span>
+                            <input type="text" id="us" name="us"/>
+                            <span class="err" id="eUS"></span>
+                        </div>
+                        </th>
+                    </tr>
+
+                    <tr>
+                        <th class="right">
+                        <div>
+                            <span class="lable">Mật Khẩu:</span>
+                            <input type="password" id="ps" name="ps"/>
+                            <span class="err" id="ePS"></span>
+                        </div>
+                        </th>
+                    </tr>
+
+                    <tr>
+                        <th class="right">
+                            <div>
+                                <span class="lable">Nhập lại mật khẩu:</span>
+                                <input type="password" id="us" name="rps"/>
+                                <span class="err" id="eRPS"></span>
+                            </div>
+                         </th>
+                    </tr>
+
+                    <tr>
+                        <th class="right"> 
+                            <div>
+                                 <span class="lable">Tên hiển thị:</span>
+                                 <input type="text" id="name" name="name"/>
+                                 <span class="err" id="eNAME"></span>
+                            </div>
+                        </th>
+                    </tr>
+
+
+                    <tr>
+                        <th class="right">
+                            <div>
+                                <span class="lable">Địa chỉ:</span>
+                                <input type="text" id="add" name="add"/>
+                                <span class="err" id="eADD"></span>
+                            </div>
+                        </th>
+                    </tr>
+
+
+                    <tr>
+                      
+                        <th class="right">
+                            <div>
+                               <span class="lable">Email:</span>
+                                <input type="text" id="mail" name="mail"/>
+                                <span class="err" id="eMail"></span>
+                            </div>
+                        </th>
+                    </tr>
+
+
+                    <tr>
+                        <th class="right">
+                            <div>
+                                <span class="lable"></span>
+                                <input type="submit" value="Đăng ký"/>     
+                            </div>
+                        </th>
+                    </tr>
+                </table>
+    </form>
+</div>
 <script type="text/javascript">
     function KiemTra(){
         var co=true;
