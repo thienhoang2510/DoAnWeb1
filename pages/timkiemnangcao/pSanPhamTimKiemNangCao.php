000eb03ce684254db77611be4e2f263d498b2934
Grinfs <47818920+grinfs@users.noreply.github.com>
2021-01-10 17:21:48 +0700

    Add files via upload

--- pages/timkiemnangcao/pSanPhamTimKiemNangCao.php
+++ pages/timkiemnangcao/pSanPhamTimKiemNangCao.php
@@ -25,11 +25,45 @@ if (isset($_POST['searchTxt'])) { //nội dung tìm kiếm
         <hr class="w-50">
         <div class="row" id="data">
             <?php
+            // TÌM TỔNG SỐ RECORDS
+            $sql = "select count(MaSanPham) as total from sanpham";
+            $result = DataProvider::ExecuteQuery($sql);
+            $row = mysqli_fetch_array($result);
+            $total_records = $row['total'];
+            // BƯỚC 3: TÌM LIMIT VÀ CURRENT_PAGE
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $limit = 9;
+            // BƯỚC 4: TÍNH TOÁN TOTAL_PAGE VÀ START
+            // tổng số trang
+            $total_page = ceil($total_records / $limit);
+    
+            // Giới hạn current_page trong khoảng 1 đến total_page
+            if ($current_page > $total_page){
+                $current_page = $total_page;
+            }
+            else if ($current_page < 1){
+                $current_page = 1;
+            }
+    
+            // Tìm Start
+            $start = ($current_page - 1) * $limit;
+    
+            $sql = "SELECT * FROM sanpham LIMIT $start, $limit";
+            $result = DataProvider::ExecuteQuery($sql);
+
             while ($row = mysqli_fetch_array($result)) {
                 $itemcard = new ItemCard($row["MaSanPham"], $row["HinhURL"], $row["TenSanPham"], $row["GiaSanPham"]);
                 $itemcard->create();
             }
             ?>
+        <?php
+        include "modules/mPagenumber.php";
+        ?>
         </div>
+
+        <!-- Phân trang -->
+
+        <hr class="w-50">
+
     </div>
 </div>
