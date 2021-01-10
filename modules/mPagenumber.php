d7793bfd784b12b0cb111fdd5c2812693e4b4132
Grinfs <47818920+grinfs@users.noreply.github.com>
2021-01-10 17:20:36 +0700

    Add files via upload

---
+++ modules/mPagenumber.php
@@ -0,0 +1,19 @@
+  <div class="pagination">
+           <?php 
+
+           ?>
+            <nav aria-label="Page navigation">
+              <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=1">1</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=2">2</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=3">3</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=4">4</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=5">5</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=6">6</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=7">7</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=8">8</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=9">9</a></li>
+                <li class="page-item"><a class="page-link" href="index.php?layout=1&side=2&content=5&page=10">10</a></li>
+              </ul>
+            </nav>
+        </div>
